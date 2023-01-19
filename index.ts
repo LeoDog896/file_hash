2954b30732c61c466da938eb16ec4acd880a7b7f
Tristan F <LeoDog896@hotmail.com>
2023-01-18 20:44:14 -0500

    feat: finalize MVP endpoints

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
-import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+import { Application, HttpException } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+import { FormFile } from "https://deno.land/x/abc@v1.3.3/vendor/https/deno.land/std/mime/multipart.ts";
+import { hashFromContent } from "./hash.ts";
 
 const app = new Application();
 
@@ -15,6 +17,33 @@ app
     return c.file(filePath);
   })
   .put("/put/", async (c) => {
+    const { file } = await c.body as { file: FormFile };
+    
+    const hash = await hashFromContent(file.content);
+
+    const filePath = `${filesDir}/${hash}`;
+
+    try {
+      await Deno.writeFile(filePath, file.content ?? new Uint8Array(), { createNew: true });
+    } catch (e) {
+      if (e instanceof Deno.errors.AlreadyExists) {
+        throw new HttpException("File already exists", 409);
+      }
+
+      throw e;
+    }
+  })
+  .delete("/delete/:hash", (c) => {
+    const hash = c.params.hash;
+    const filePath = `${filesDir}/${hash}`; // path traversal vulnerability?
+    
+    try {
+      Deno.removeSync(filePath);
+    } catch (e) {
+      if (e instanceof Deno.errors.NotFound) {
+        throw new HttpException("File not found", 404);
+      }
+    }
   })
   .get("/list", () => {
     const files = Deno.readDirSync(filesDir);
